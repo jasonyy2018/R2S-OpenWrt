69f582fe75009e86a8bd163d191727e1f51d9c9c
msylgj <msylgj@vip.qq.com>
2020-11-19 16:40:45 +0800

    rx tx off

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -75,6 +75,8 @@ wget -P target/linux/rockchip/patches-5.4/ https://raw.githubusercontent.com/Qiu
 #IRQ
 sed -i '/;;/i\set_interface_core 8 "ff160000" "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 sed -i '/;;/i\set_interface_core 1 "ff150000" "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+#disabed rk3328 ethernet tcp/udp offloading tx/rx
+sed -i '/;;/i\ethtool -K eth0 rx off tx off && logger -t disable-offloading "disabed rk3328 ethernet tcp/udp offloading tx/rx"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 #SWAP LAN WAN
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
